ecfc28d103abf42f6925b0e59dfc7b9f7df063c1
gtlyy <49363208+gtlyy@users.noreply.github.com>
2025-04-06 10:07:59 +0800

    upload files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿
+namespace scanFile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 92);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(727, 346);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(13, 23);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(589, 21);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(622, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "选择文件夹";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.Description = "请选择文件夹，并点击确定。";
+            this.folderBrowserDialog1.ShowNewFolderButton = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 462);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(677, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "功能：查找名称过长的文件（夹）。\r\n说明：一般而言，Windows编码为GBK，1个中文占2个字节，而Linux编码为UTF8，1个中文占3个字节，但文件名最多2" +
+    "55个字节。";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 12);
+            this.label2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(749, 506);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.ImeMode = System.Windows.Forms.ImeMode.KatakanaHalf;
+            this.Name = "Form1";
+            this.Text = "ScanFile-V220917";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+
+namespace scanFile
+{
+    public partial class Form1 : Form
+    {
+        static int num = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void FindFile(string url)
+        {
+            DirectoryInfo directory;
+            // 打开 “百度网盘同步空间”时出错！略过。
+            try {
+                directory = new DirectoryInfo(url);
+            }
+            catch(Exception e)
+            {
+                textBox1.Text = "Error: new DirectoryInfo()" + "\r\n" + e.ToString() + "\r\n\r\n" + "请重新选择文件夹";
+                return;
+            }
+            // 首先判断文件夹名是否过长
+            if (System.Text.Encoding.UTF8.GetByteCount(directory.Name) > 255)
+            {
+                num += 1;
+                textBox1.Text += num.ToString() + "【Dir】 : " + directory.FullName + System.Environment.NewLine + System.Environment.NewLine;
+            }
+
+            // 打开虚拟机的共享文件夹时，linux下文件夹可以有空格结尾，但windows明显不行。
+            try { 
+                var files = directory.GetFiles();
+                if (files.Any())
+                {
+                    foreach (var file in files)
+                    {
+                        // 判断文件名是否过长
+                        int lenght1 = System.Text.Encoding.UTF8.GetByteCount(file.Name);
+                        if (lenght1 > 255)
+                        {
+                            num += 1;
+                            textBox1.Text += num.ToString() + "【File】 : " + file.FullName + System.Environment.NewLine + System.Environment.NewLine;
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                textBox1.Text = "Error: directory.GetFiles()" + "\r\n" + e.ToString() + "\r\n\r\n" + "请重新选择文件夹";
+                return;
+            }
+
+            var dirs = directory.GetDirectories();
+            if (dirs.Any())
+            {
+                foreach (var dir in dirs)
+                {
+                    FindFile(dir.FullName);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    textBox2.Text = folderBrowserDialog1.SelectedPath;
+                    num = 0;
+                    textBox1.Text = "";
+                    label2.Text = "";
+                    FindFile(textBox2.Text);
+                    label2.Text = "名称过长的文件（夹）有 " + num.ToString() + " 个：";
+                }
+            }
+            catch (Exception ex)
+            {
+                textBox1.Text = "Error: button1_Click()" + "\r\n" + ex.ToString() + "\r\n\r\n" + "请重新选择文件夹";
+                return;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
